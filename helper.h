cff17b618f876da6be59aa8bfc501a1dac8865e2
Jordan <jordannguyen95@gmail.com>
2016-01-20 22:10:12 -0800

    Pair class is implemented

--- helper.h
+++ helper.h
@@ -29,6 +29,17 @@ struct compareY
 	}
 };
 
+struct comparePairs
+{
+	inline bool operator() (const Pair &pair1, const Pair &pair2)
+	{
+		if (pair1.p1.x < pair2.p2.x)
+			return true;
+		else if (pair1.p1.x == pair2.p2.x)
+			return (pair1.p1.y <= pair2.p2.y);
+	}
+};
+
 struct compareXPairs
 {
 	inline bool operator() (const Pair &pair1, const Pair &pair2)
@@ -97,14 +108,16 @@ double distance(Point p1, Point p2) {
 	return sqrt(xs + ys);
 }
 
-void printPairs(std::vector<Point> pairs, double dist) {
+void printPairs(std::vector<Pair> pairs, double dist) {
 	
 	std::cout.precision(7);
+	std::cout << "number of pairs: " << pairs.size() << std::endl;
 	std::cout << "closest pair distance: " << dist << std::endl;
-	for (int i = 0; i < pairs.size(); i = i+2) {
+	std::sort(pairs.begin(), pairs.end(), comparePairs());
+	for (int i = 0; i < pairs.size(); i++) {
 		pairs[i].toString();
-		std::cout << " ";
-		pairs[i+1].toString();
+		//std::cout << " ";
+		//pairs[i+1].toString();
 		std::cout << std::endl;
 	}
 }
@@ -112,70 +125,64 @@ void printPairs(std::vector<Point> pairs, double dist) {
 /* need to implement way to put pairs in order into vector*/
 void bruteForce(std::vector<Point> points) {
 	
-	std::vector<Point> pair;
+	std::vector<Pair> pairs;
 	double minDistance = DBL_MAX;
 	int n = points.size();
 	for (int i = 0; i < n; i++) {
 		for (int j = i+1; j < n; j++) {
 			if (distance(points[i],points[j]) < minDistance) //new min found, clear vector
 			{
+				std::cout << "new min dist found" << std::endl;
 				minDistance = distance(points[i], points[j]);
-				pair.clear();
+				pairs.clear();
 				if (points[i].x < points[j].x) //compare x values. i then j
 				{
-					pair.push_back(points[i]);
-					pair.push_back(points[j]);
+					pairs.push_back(Pair(points[i], points[j]));
 				}
 				else if (points[i].x > points[j].x) //j then i
 				{
-					pair.push_back(points[j]);
-					pair.push_back(points[i]);
+					pairs.push_back(Pair(points[j], points[i]));
 				}
 				else if (points[i].x == points[j].x && points[i].y < points[j].y) //compare y values if x are same
 				{
-					pair.push_back(points[i]);
-					pair.push_back(points[j]);
+					pairs.push_back(Pair(points[i], points[j]));
 				}
 				else if (points[i].x == points[j].x && points[i].y > points[j].y) 
 				{
-					pair.push_back(points[j]);
-					pair.push_back(points[i]);
+					pairs.push_back(Pair(points[j], points[i]));
 				}
 			}
 
-			else if (distance(points[i],points[j]) == minDistance) //another min pair found, add to vector
+			else if (fabs(distance(points[i],points[j]) - minDistance) < .0000001) //another min pair found, add to vector
 			{
+				std::cout << "same min dist found" << std::endl;
 				if (points[i].x < points[j].x) //compare x values. i then j
 				{
-					pair.push_back(points[i]);
-					pair.push_back(points[j]);
+					pairs.push_back(Pair(points[i], points[j]));
 				}
 				else if (points[i].x > points[j].x) //j then i
 				{
-					pair.push_back(points[j]);
-					pair.push_back(points[i]);
+					pairs.push_back(Pair(points[j], points[i]));
 				}
 				else if (points[i].x == points[j].x && points[i].y < points[j].y) //compare y values if x are same
 				{
-					pair.push_back(points[i]);
-					pair.push_back(points[j]);
+					pairs.push_back(Pair(points[i], points[j]));
 				}
 				else if (points[i].x == points[j].x && points[i].y > points[j].y) 
 				{
-					pair.push_back(points[j]);
-					pair.push_back(points[i]);
+					pairs.push_back(Pair(points[j], points[i]));
 				}
 			}
 		}
 	}	
 	
-	return printPairs(pair, minDistance);
+	return printPairs(pairs, minDistance);
 }
 
-// double basicSort(std::vector<Point> points)
-// {
-// 	points.sort(points.begin(), points.)
-// }
+double basicSort(std::vector<Point> points)
+{
+	points.sort(points.begin(), points.)
+}
 
 
 
